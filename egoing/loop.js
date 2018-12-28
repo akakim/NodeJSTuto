0ef62f4c5354df5fd1b9abf4382a913cee6105a5
akakim <daphne5009@gmail.com>
2018-12-28 18:02:10 +0900

    생활코딩 업로드 루프문, html 자동생성기.

---
+++ egoing/loop.js
@@ -0,0 +1,22 @@
+//console.log('A');
+//console.log('B');
+//console.log('C');
+//console.log('D');
+
+//var i  = 0;
+/*
+while ( i < 4 ) {
+    console.log( i );
+    i = i+1;
+}*/
+
+var mArr = ['a','b','c','d','e',12];
+
+mArr.concat('aaaaa');
+var k = 0;
+while( k < mArr.length ){
+
+    console.log ( `${k}  : ${mArr[k]}`);
+    k+=1;
+
+}
